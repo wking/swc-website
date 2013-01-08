26bbd09d1a8ca7a9326dafbe80d928c1abdc0045
W. Trevor King <wking@tremily.us>
2013-01-08 14:31:36 -0500

    generators: Tweak TemplatePagesGenerator to expand template content
    This allow you to use |filename| linking in your template pages (but
    not in the direct templates living in the theme).

--- pelican/readers.py
+++ pelican/readers.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import codecs
 import datetime
 import logging
 import os
@@ -91,6 +92,22 @@ class PelicanHTMLTranslator(HTMLTranslator):
 
     def depart_abbreviation(self, node):
         self.body.append('</abbr>')
+
+
+class RawReader(Reader):
+    enabled = True
+    file_extensions = ['raw']
+
+    def read(self, source_path, encoding=None):
+        "Read file contents into .content without processing"
+        if not encoding:
+            encoding = self.settings.get('CONTENT_ENCODING', None)
+        if encoding:
+            content = codecs.open(source_path, 'r', encoding=encoding).read()
+        else:
+            content = open(source_path, 'rb').read()
+        metadata = {}
+        return content, metadata
 
 
 class RstReader(Reader):
